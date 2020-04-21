fd2d0c1b97f7f9f115d36f079126711a568b177c
harsh aggarwal <harshaggarwal060@gmail.com>
2020-04-21 10:03:43 +0530

    fixed mongoose

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://http://15.206.74.7/goHidden');
+mongoose.connect('mongodb://localhost/goHidden');
 
 const db = mongoose.connection;
 
